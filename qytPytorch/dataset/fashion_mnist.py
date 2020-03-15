99d9f817b56108b6fbd12da211e413fc38fba70b
759729997 <qiaoyongtian@qq.com>
2020-03-15 15:24:15 +0800

    展示图像与标签

--- qytPytorch/dataset/fashion_mnist.py
+++ qytPytorch/dataset/fashion_mnist.py
@@ -3,9 +3,32 @@
 
     Main members:
 
+        # get_dataset - 获取数据集.
         # get_labels_by_ids - 根据标签id获取标签具体描述.
         # show_fashion_mnist - 展示图像与标签.
 """
+import torchvision
+from matplotlib import pyplot as plt
+
+from qytPytorch import logger
+
+
+def get_dataset(data_path):
+    """ 获取数据集.
+
+        @params:
+            data_path - 数据保存路径.
+
+        @return:
+            On success - train与test数据.
+            On failure - 错误信息.
+    """
+    mnist_train = torchvision.datasets.FashionMNIST(root=data_path, train=True, download=True, transform=torchvision.transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(root=data_path, train=False, download=True, transform=torchvision.transforms.ToTensor())
+    logger.info('dataset is :{}'.format(type(mnist_train)))
+    logger.info('train data len :{}'.format(len(mnist_train)))
+    logger.info('test data len :{}'.format(len(mnist_test)))
+    return mnist_train, mnist_test
 
 
 def get_labels_by_ids(label_ids, return_Chinese=False):
@@ -35,8 +58,8 @@ def show_fashion_mnist(images, labels):
             images - 图像特征列表.
             labels - 图像标签列表.
     """
-    d2l.use_svg_display()
-    # 这里的_表示我们忽略（不使用）的变量
+    # use_svg_display()
+    # _表示我们忽略（不使用）的变量
     _, figs = plt.subplots(1, len(images), figsize=(12, 12))
     for f, img, lbl in zip(figs, images, labels):
         f.imshow(img.view((28, 28)).numpy())
--- test/dataset/test_fashion_mnist.py
+++ test/dataset/test_fashion_mnist.py
@@ -12,17 +12,31 @@ sys.path.insert(0, './')  # 定义搜索路径的优先顺序，序号从0开始
 
 import qytPytorch  # noqa
 print('qytPytorch module path :{}'.format(qytPytorch.__file__))  # 输出测试模块文件位置
+from qytPytorch.dataset.fashion_mnist import get_dataset  # noqa
 from qytPytorch.dataset.fashion_mnist import get_labels_by_ids  # noqa
+from qytPytorch.dataset.fashion_mnist import show_fashion_mnist  # noqa
 
 
 class TestFashionMnist(unittest.TestCase):
     """ FashionMnist数据集常用函数.
 
     Main methods:
+        test_get_dataset - 获取数据集.
         test_get_labels_by_ids - 根据标签id获取标签具体描述.
+        test_show_fashion_mnist - 展示图像与标签.
     """
 
-    # @unittest.skip('debug')
+    @unittest.skip('debug')
+    def test_get_dataset(self):
+        """ 获取数据集.
+        """
+        print('{} test_get_dataset {}'.format('-'*15, '-'*15))
+        data_path = './data/FashionMNIST'
+        mnist_train, mnist_test = get_dataset(data_path=data_path)
+        feature, label = mnist_train[0]
+        print(feature.shape, label)  # torch.Size([1, 28, 28]) 9
+
+    @unittest.skip('debug')
     def test_get_labels_by_ids(self):
         """ 根据标签id获取标签具体描述.
         """
@@ -30,6 +44,20 @@ class TestFashionMnist(unittest.TestCase):
         label_ids = [1, 5, 3]
         print(get_labels_by_ids(label_ids))  # ['trouser', 'sandal', 'dress']
         print(get_labels_by_ids(label_ids, return_Chinese=True))  # ['裤子', '凉鞋', '连衣裙']
+
+    # @unittest.skip('debug')
+    def test_show_fashion_mnist(self):
+        """ 展示图像与标签.
+        """
+        print('{} test_show_fashion_mnist {}'.format('-'*15, '-'*15))
+        data_path = './data/FashionMNIST'
+        mnist_train, mnist_test = get_dataset(data_path=data_path)
+        feature, label = mnist_train[0]
+        X, y = [], []
+        for i in range(10):
+            X.append(mnist_train[i][0])
+            y.append(mnist_train[i][1])
+        show_fashion_mnist(X, get_labels_by_ids(y))   # 直接弹出图片展示页面
 
 
 if __name__ == "__main__":
